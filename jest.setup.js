36937f7658b863bed4c9fbe2957ee1050ba3c014
ramiel <ramielwan48@gmail.com>
2023-12-20 11:39:18 +0300

    posthog with async storage mock

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import {jest} from '@jest/globals';
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
